de5b33398036f5f5e7402085850e1fee2bdcc8d4
Sessy24 <cozynook24@gmail.com>
2021-05-22 11:29:30 +0900

    (debug_false

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+rpg#n2h&$0c0j&ejm9qwfisez8msyuj3c7c3dv!===sq=%41x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
